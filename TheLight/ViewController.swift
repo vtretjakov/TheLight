c1d13a59a5efeade2a2d4e631f96049d05a1d827
Vladimir Tretyakov <vladimir.tretjakoff@yandex.ru>
2021-05-02 06:31:43 +0700

    Added button and switch function

---
+++ TheLight/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TheLight
+//
+//  Created by Владимир on 02.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var isLightOn = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        }
+
+    fileprivate func updateUI() {
+        
+        if isLightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        } // можно так
+        
+        view.backgroundColor = isLightOn ? .white : .black
+    } // можно так (короткая версия)
+    
+    @IBAction func ButtonPressed() {
+        isLightOn.toggle()
+        updateUI()
+    }
+    
+}
+
